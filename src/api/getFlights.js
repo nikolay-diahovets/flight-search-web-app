2448e71d83bc0b09360b0e85d612fbd0458d86d9
Nikolay Diahovets <n.diahovets@do-it.co>
2020-08-26 14:02:11 +0300

    add poll flights before get flights;

--- src/api/getFlights.js
+++ src/api/getFlights.js
@@ -3,6 +3,10 @@ import queryString from "query-string";
 export const getFlights = async (queryObject) => {
   const query = queryString.stringify(queryObject, { skipNull: true, skipEmptyString: true });
   const url = `http://business-issue1608.test.travelwits.com/getflights?${query}`;
+
+  const pollUrl = `http://business-issue1608.test.travelwits.com/pollflights?${query}`;
+  await fetch(pollUrl);
+
   const response = await fetch(url);
   return await response.json();
 };
